4f8e30be41c0c878c070fb1f52182e8f3f53322d
Ethan Swan <eswan18@rocketmail.com>
2018-10-13 21:17:40 -0400

    some cleanup and comments on assignment 1

--- assignment_1/main.cpp
+++ assignment_1/main.cpp
@@ -22,13 +22,14 @@ int main(int argc, char* argv[]){
         return 1;
     }
     
+    // Read in the infile and parse the grammar.
     string infile_name(argv[1]);
     map<string, vector<string> > grammar;
     grammar = parse_grammar(infile_name);
 
     map<string, vector<string> >::const_iterator it;
     vector<string>::const_iterator iv;
-    // Recursively generate a sentence starting with nonterminal <start>
+    // Recursively generate a sentence starting with nonterminal <start>.
     string random_sentence = fill_nonterminal("start", grammar);
     cout << random_sentence << endl;
 }
@@ -37,8 +38,8 @@ map<string, vector<string> > parse_grammar(string filename) {
     ifstream file(filename);
     string line;
     map<string, vector<string> > result;
+    // Iterate through the file.
     while (getline(file, line)) {
-        //istringstream iss(line);
         // Left brace means we start a new non-terminal
         if (line[0] == '{') {
             string key;
@@ -52,12 +53,13 @@ map<string, vector<string> > parse_grammar(string filename) {
             // The following lines, until '}', will be expansions
             getline(file, line);
             while (line[0] != '}') {
-                // Remove leading and trailing spaces, and ;
+                // Remove leading and trailing spaces, and ';'.
                 line = regex_replace(line, regex("^ +| +$|( ) +"), "$1");
                 line.erase(std::remove(line.begin(), line.end(), ';'), line.end());
                 value.push_back(line);
                 getline(file, line);
             }
+            // "Save" the rule in the map.
             result[key] = value;
         }
     }
@@ -71,18 +73,20 @@ string fill_nonterminal(string nonterminal, map<string, vector<string> > grammar
     vector<string> productions = grammar[nonterminal];
     vector<string>::const_iterator iv;
     string result;
-    // Choose a production at random and iterate through it.
+    // Choose a production at random.
     int n_productions = productions.size();
-    std::random_device rd;     // only used once to initialise (seed) engine
-    std::mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
-    std::uniform_int_distribution<int> uni(0,n_productions-1); // guaranteed unbiased
+    // Only used once to initialise (seed) engine.
+    std::random_device rd;
+    // Random-number engine (Mersenne-Twister in this case).
+    std::mt19937 rng(rd());
+    std::uniform_int_distribution<int> uni(0,n_productions-1);
     int choice = uni(rng);
     string prod = productions[choice];
-    // Iterate through the production and deal with nonterminals.
+    // Iterate through the production and deal with nonterminals as we find them.
     string::const_iterator it;
     for (it = prod.begin(); it != prod.end(); it++ ) {
-        // If we encounter a '<', we need to parse a nonterminal.
         if (*it == '<') {
+        // If we encounter a '<', we need to parse a nonterminal.
             string nonterm;
             // Catch all the letters of the nonterminal.
             it++;
@@ -90,9 +94,10 @@ string fill_nonterminal(string nonterminal, map<string, vector<string> > grammar
                 nonterm += *it;
                 it++;
             }
-            // Call fill_nonterminal on this nonterminal
+            // Call fill_nonterminal (recursion!) on this nonterminal.
             result += fill_nonterminal(nonterm, grammar);
         } else {
+            // If what we found wasn't a nonterminal, just print it and move on.
             result += *it;
         }
     }
